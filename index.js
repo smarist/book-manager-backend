8536405f0576416db335573df0c158f91a9f45d9
smarist <stmarisnwokike@gmail.com>
2023-03-04 16:05:00 +0100

    added image upload function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import multer from "multer"
 import mysql2 from "mysql2";
 import cors from "cors";
 
 const app = express();
+// const multer  = require('multer');
 const db = mysql2.createConnection({
   host: "localhost",
   user: "root",
@@ -10,7 +12,11 @@ const db = mysql2.createConnection({
   database: "test",
 });
 
+const upload = multer({ dest: 'uploads/' });
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -25,13 +31,41 @@ app.get("/books", (req, res) => {
   });
 });
 
-app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`, `desc`,`cover`, `price`) VALUES (?)";
-  const values = [req.body.title, req.body.desc, req.body.cover, req.body.price];
-  db.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Book has been created");
+app.get("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "SELECT * FROM books WHERE id = ?"
+    db.query(q, [bookId], (err, data) => {
+      if (err) return res.json(err);
+      return res.json(data);
+    });
+});
+
+app.post("/books", upload.single('cover'), (req, res) => {
+    const q = "INSERT INTO books (`title`, `desc`, `cover`, `price`) VALUES (?)";
+    const values = [req.body.title, req.body.desc, req.file.path, req.body.price];
+    db.query(q, values, (err, data) => {
+      if (err) return res.json(err);
+      return res.json("Book has been created");
+    });
   });
+
+app.put("/books/:id", upload.single('cover'), (req, res) => {
+    const bookId = req.params.id;
+    const q = "UPDATE books SET `title` = ?, `desc` = ?,`cover` = ?, `price` = ? WHERE id = ?";
+    const values = [req.body.title, req.body.desc, req.body.cover, req.body.price];
+    db.query(q, [...values, bookId], (err, data) => {
+      if (err) return res.json(err);
+      return res.json("Book has been Updated Successfully");
+    });
+});
+
+app.delete("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const q = "DELETE FROM books WHERE id = ?"
+    db.query(q, [bookId], (err, data) => {
+      if (err) return res.json(err);
+      return res.json("Book has been deleted");
+    });
 });
 
 app.listen(8800, () => {
